42287a790045bcd4402e82b8e80852ca5c2ecd8d
naye <0622kimny@naver.com>
2025-06-11 17:01:27 +0900

    ALLOWED_HOSTS 수정

--- gostop_project/settings.py
+++ gostop_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['gostop-calculator.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
