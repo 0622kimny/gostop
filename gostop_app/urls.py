e9e40e9f19cf8757924e33883436082a1d0eaf09
naye <0622kimny@naver.com>
2025-06-11 18:05:36 +0900

    구글 사이트맵 오류 수정

--- gostop_app/urls.py
+++ gostop_app/urls.py
@@ -1,14 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.sitemaps.views import sitemap
-from gostop_app.sitemaps import StaticViewSitemap
-
-sitemaps = {
-    'static': StaticViewSitemap,
-}
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('gostop_app.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('', views.setup_view, name='setup'),
+    path('calculate/', views.calculate_view, name='calculate'),
 ]
--- gostop_project/urls.py
+++ gostop_project/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from gostop_app.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gostop_app.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
