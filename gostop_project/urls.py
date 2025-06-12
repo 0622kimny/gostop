8b646f6be746b849c77dc3dc894ca6064c6040ae
naye <0622kimny@naver.com>
2025-06-12 09:40:33 +0900

    ads.txt 추가

--- gostop_project/urls.py
+++ gostop_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from gostop_app.sitemaps import StaticViewSitemap
+from django.views.generic.base import TemplateView
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -11,4 +12,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gostop_app.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("ads.txt", TemplateView.as_view(template_name="ads.txt", content_type="text/plain")),
 ]
